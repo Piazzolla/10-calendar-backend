dce9b9d7a7f848d4fcfb9efdd73eef3c9c1b549f
Mariano <macacozzz@hotmail.com>
2022-09-23 16:30:29 -0300

    prueba

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnection = async() => {
     try {
-        await mongoose.connect(process.env.DB_CNN);
+        await mongoose.connect(process.env.DB_CNN, { useNewUrlParser: true });
         console.log('DB Online');
     } catch (error) {
         console.log(error);
