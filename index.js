03761c607ac0e3d0afea318c8e4596f05f153cce
Mariano <macacozzz@hotmail.com>
2022-09-26 11:06:53 -0300

    agrego comodin para las rutas

--- index.js
+++ index.js
@@ -25,6 +25,10 @@ app.use( express.json() );
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
+app.get('*', ( req, res ) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
 // TODO: CRUD: Eventos
 
 // Escuchar peticiones
